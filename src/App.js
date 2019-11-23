53356cfb713729d843f0599b01aade855196a917
zuriros <hanami33rosas@gmail.com>
2019-11-22 20:03:30 -0500

    practicando con react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+     super(props)
+     this.state = {
+        newTask: "hola",
+     }
+  }
+
+  handleTaskChange = (event) => {
+    this.setState({
+       newTask: event.target.value,
+    })
+ }
+
+render() {
+     return (
+        <div className="container">
+           <h1 className="title">
+              Hello world
+              <span aria-label="emoji" role="img">🔥</span>
+           </h1>
+           <input onChange={this.handleTaskChange} type="text" className="new-task"/>
+           <h2 className="test-label">{this.state.newTask}</h2>
+
+        </div>
+     );
+  }
 }
 
 export default App;
